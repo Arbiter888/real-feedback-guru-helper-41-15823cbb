cf04ab3611cc371062cfe2750b55483b2dcdc165
Alex Araiza <alexandre.araiza@gmail.com>
2024-12-28 23:50:56 +0100

    Put email content and footer in same element
    [skip gpt_engineer]

--- src/components/demo/email/EmailCompositionForm.tsx
+++ src/components/demo/email/EmailCompositionForm.tsx
@@ -111,7 +111,7 @@ export const EmailCompositionForm = ({ onSend, disabled, restaurantInfo }: Email
 
     setIsSending(true);
     try {
-      await onSend(emailSubject, htmlContent || emailContent + footerHtml);
+      await onSend(emailSubject, '<div>' + (htmlContent || emailContent) + footerHtml + '</div>');
       setEmailSubject("");
       setEmailContent("");
       setHtmlContent("");
@@ -149,7 +149,7 @@ export const EmailCompositionForm = ({ onSend, disabled, restaurantInfo }: Email
         body: {
           to: testEmail,
           subject: emailSubject,
-          htmlContent: htmlContent || emailContent + footerHtml,
+          htmlContent: '<div>' + (htmlContent || emailContent) + footerHtml + '</div>',
           restaurantInfo,
         },
       });
